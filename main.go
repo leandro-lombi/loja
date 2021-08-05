1c0c2a18cb02c89f3c0270c935007d064292de42
leandro.lomb <leandro.lombi@synchro.com.br>
2021-08-05 14:50:11 -0300

    Adicionado uma list de produtos para ser exibido no index de forma dinamica

--- main.go
+++ main.go
@@ -5,15 +5,30 @@ import (
 	"text/template"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 // Essa função Must encapsula todos os templates
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
-func main()  {
+func main() {
 	// Toda requisição para o /, quem irá atender será o index
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request)  {
-	templates.ExecuteTemplate(w, "Index", nil)
+func index(w http.ResponseWriter, r *http.Request) {
+	// slice de produto
+	produtos := []Produto{
+		{"Camiseta", "Algodão", 29.9, 2},
+		{"Tenis", "Azul", 349.5, 4},
+		{"Meia", "Branca", 8.49, 15},
+		{"Sapato", "Marrom", 143.75, 6},
+	}
+
+	templates.ExecuteTemplate(w, "Index", produtos)
 }
