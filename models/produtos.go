2c62928d19f5b9e5a3f7d1ba1b0c45298be7dd12
leandro.lomb <leandro.lombi@synchro.com.br>
2021-08-05 15:46:18 -0300

    Modularizado o codigo

---
+++ models/produtos.go
@@ -0,0 +1,43 @@
+package models
+
+import "github.com/lnl/loja/db"
+
+type Produto struct {
+	Id int
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+func BuscaTodosOsProdutos() []Produto {
+	db := db.ConectaComBancoDeDados()
+
+	selectTodosProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectTodosProdutos.Next(){
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectTodosProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+	defer db.Close()
+	return produtos
+}
